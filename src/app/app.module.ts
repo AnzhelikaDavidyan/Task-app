6a06c689aea67293fb4c7e6d4d045e59ecee2baf
Anzhelika <anzhelika.davidyan98@gmail.com>
2022-09-09 01:36:15 +0400

    fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './controls/header/header.component';
-import { BooksTableComponent } from './controls/books-table/books-table.component';
-import { MaterialModule } from './material.module';
-import { GenresTableComponent } from './controls/genres-table/genres-table.component';
-import { AuthorsTableComponent } from './controls/authors-table/authors-table.component';
 import { AuthorsModule } from './controls/authors-table/authors.module';
+import { BooksTableComponent } from './controls/books-table/books-table.component';
 import { GenresModule } from './controls/genres-table/genres.module';
+import { HeaderComponent } from './controls/header/header.component';
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
